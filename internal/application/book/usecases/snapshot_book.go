caae2dc21baf0e85be4e70cdb0345860221e05bb
Junior <juninhoitabh@gmail.com>
2025-08-16 01:34:50 +0000

    feat: implement SnapshotBookUseCase for retrieving snapshot of bids and asks

---
+++ internal/application/book/usecases/snapshot_book.go
@@ -0,0 +1,58 @@
+package book
+
+import (
+	domainBook "github.com/juninhoitabh/clob-go/internal/domain/book"
+	"github.com/juninhoitabh/clob-go/internal/shared"
+)
+
+type SnapshotBookOutput struct {
+	Instrument string
+	Bids       []Level
+	Asks       []Level
+}
+
+type Level struct {
+	Price int64
+	Qty   int64
+}
+
+type SnapshotBookUseCase struct {
+	BookRepo domainBook.BookRepository
+}
+
+func (uc *SnapshotBookUseCase) Execute(instrument string) (*SnapshotBookOutput, error) {
+	b, err := uc.BookRepo.GetBook(instrument)
+	if err != nil {
+		return nil, err
+	}
+
+	if b == nil {
+		return nil, shared.ErrNotFound
+	}
+
+	out := &SnapshotBookOutput{
+		Instrument: instrument,
+		Bids:       []Level{},
+		Asks:       []Level{},
+	}
+
+	for _, p := range b.BidPrices() {
+		pl := b.Bids()[p]
+		q := pl.TotalQty()
+
+		if q > 0 {
+			out.Bids = append(out.Bids, Level{Price: p, Qty: q})
+		}
+	}
+
+	for _, p := range b.AskPrices() {
+		pl := b.Asks()[p]
+		q := pl.TotalQty()
+
+		if q > 0 {
+			out.Asks = append(out.Asks, Level{Price: p, Qty: q})
+		}
+	}
+
+	return out, nil
+}
