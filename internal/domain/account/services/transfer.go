e69a03b6c0a0d37947ee11ed0bf6689ba78cf864
Junior <juninhoitabh@gmail.com>
2025-08-16 00:53:27 +0000

    refactor: remove commented-out Transfer method from transfer.go

--- internal/domain/account/services/transfer.go
+++ internal/domain/account/services/transfer.go
@@ -13,16 +13,3 @@ func Transfer(from, to *account.Account, asset string, amount int64) error {
 
 	return to.Credit(asset, amount)
 }
-
-// TODO: Era assim, ver como vai ficar
-// func (i *InMemoryAccountsRepository) Transfer(receiver, asset string, amount int64) error {
-// 	i.mu.Lock()
-// 	defer i.mu.Unlock()
-
-// 	acct, ok := i.accounts[receiver]
-// 	if !ok {
-// 		return shared.ErrNotFound
-// 	}
-
-// 	return acct.Credit(asset, amount)
-// }
