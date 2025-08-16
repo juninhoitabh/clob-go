fa18539229fc8e62fda946226b22e2b6d0302030
Junior <juninhoitabh@gmail.com>
2025-08-16 19:03:45 +0000

    feat: implement CreateAccount use case with structured input and output

---
+++ internal/application/account/usecases/create_account.go
@@ -0,0 +1,39 @@
+package usecases
+
+import (
+	domainAccount "github.com/juninhoitabh/clob-go/internal/domain/account"
+	idObjValue "github.com/juninhoitabh/clob-go/internal/shared/domain/value-objects/id"
+)
+
+type (
+	CreateAccountInput struct {
+		AccountName string
+	}
+	CreateAccountOutput struct {
+		ID   string
+		Name string
+	}
+	CreateAccountUseCase struct {
+		accountRepo domainAccount.IAccountRepository
+	}
+)
+
+func (c *CreateAccountUseCase) Execute(input CreateAccountInput) (*CreateAccountOutput, error) {
+
+	account, err := domainAccount.NewAccount(domainAccount.AccountProps{
+		Name: input.AccountName,
+	}, idObjValue.Uuid)
+	if err != nil {
+		return nil, err
+	}
+
+	err = c.accountRepo.Create(account)
+	if err != nil {
+		return nil, err
+	}
+
+	return &CreateAccountOutput{
+		ID:   account.GetID(),
+		Name: account.Name,
+	}, nil
+}
