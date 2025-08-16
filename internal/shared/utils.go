56534ee0400b974f53fcbbf8daf5b5dea6a16e25
Junior <juninhoitabh@gmail.com>
2025-08-16 14:15:16 +0000

    feat: add WriteJSON utility function for JSON response handling

--- internal/shared/utils.go
+++ internal/shared/utils.go
@@ -1,3 +1,13 @@
 package shared
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 func Mul(a, b int64) int64 { return a * b }
+
+func WriteJSON(w http.ResponseWriter, status int, v any) {
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(v)
+}
