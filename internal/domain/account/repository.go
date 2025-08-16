9f55a7d628560b32cbc9610249ab2b9e22f77904
Junior <juninhoitabh@gmail.com>
2025-08-16 00:32:15 +0000

    fix: rename AccountsRepository to AccountRepository for consistency

--- internal/domain/account/repository.go
+++ internal/domain/account/repository.go
@@ -1,6 +1,6 @@
 package account
 
-type AccountsRepository interface {
+type AccountRepository interface {
 	Create(id, name string) bool
 	Get(id string) (*Account, error)
 	Credit(id, asset string, amount int64) error
