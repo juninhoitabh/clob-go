d14d7469deee30389cb93b5d00285a0e3ff832a6
Junior <juninhoitabh@gmail.com>
2025-08-16 18:07:42 +0000

    refactor: rename IBookRepository to IOrderRepository for consistency

--- internal/domain/order/repository.go
+++ internal/domain/order/repository.go
@@ -1,6 +1,6 @@
 package order
 
-type IBookRepository interface {
+type IOrderRepository interface {
 	GetOrder(orderID string) (*Order, error)
 	SaveOrder(o *Order) error
 	RemoveOrder(orderID string) error
