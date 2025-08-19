ee09713f9507dcf9b372ecca727f7e88fa021413
Junior <juninhoitabh@gmail.com>
2025-08-19 09:24:19 +0000

    feat: add CORS middleware to the HTTP server router

--- internal/infra/http-server/router/router.go
+++ internal/infra/http-server/router/router.go
@@ -34,7 +34,24 @@ func Generate(apiPort string) http.Handler {
 		http.NotFound(w, r)
 	})
 
-	return withJSON(withRecover(mux))
+	return withCORS(withJSON(withRecover(mux)))
+}
+
+func withCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, X-CSRF-Token")
+		w.Header().Set("Access-Control-Max-Age", "300")
+
+		// Lidar com requisições preflight OPTIONS
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func withJSON(next http.Handler) http.Handler {
