7471a52a893b9d6fe12a5890f1ec5ef80aa7ba83
Junior <juninhoitabh@gmail.com>
2025-08-19 17:16:20 +0000

    feat: add gosweep.sh script for automated testing and coverage reporting

---
+++ gosweep.sh
@@ -0,0 +1,31 @@
+#!/bin/zsh
+set -e
+
+echo 'mode: count' > coverage.out
+
+max_steps=3
+
+for dir in $(find . -maxdepth 10 -not -path './.git*' -not -path './.history*' -path '*/internal/*' -type d);
+do
+if ls $dir/*.go &> /dev/null; then
+    if [[ $dir != (*"helper-test"*|*"factories"*|*"validators"*|*"dtos"*|*"fakers"*|*"infra/cmd"*|*"infra/framework/database"*|*"middle-wares"*|*"shared/config"*) ]]; then
+        echo "Running go test race $dir ... (1/$max_steps)"
+        ENVIRONMENT=test go test -tags=all -v -short -race "$dir"
+
+        echo "Running go test $dir ... (2/$max_steps)"
+        ENVIRONMENT=test go test -tags=all -v -short -covermode=count -coverprofile=$dir/coverage.tmp "$dir"
+        if [ -f $dir/coverage.tmp ]
+        then
+            cat $dir/coverage.tmp | tail -n +2 >> coverage.out
+            rm $dir/coverage.tmp
+        fi
+    fi
+fi
+done
+
+go tool cover -func coverage.out
+go tool cover -html=coverage.out -o coverage.html
+
+echo "Running golangci-lint $dir ... (3/$max_steps)"
+
+golangci-lint run ./...
